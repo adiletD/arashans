0cba30b1674e1e392ea3e0bfb00d603e13b6d764
“adiletD” <daniiarov.adilet@gmail.com>
2020-11-12 17:55:29 +0800

    homescreen and products with rating are setup on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1>Welcome to Proshop</h1>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,13 @@
 import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
 
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer>
+      <Row>
+        <Col className='text-center py-8'>Copyright &copy; Arashans</Col>
+      </Row>
+    </footer>
+  )
 }
 export default Footer
